aa0b58a0a1b05d6897dbc2b4b1d158f13313791d
Vasily Kazarezov <vasily.kazarezov@cgr-tech.com>
2022-06-08 14:55:55 +0200

    Network chat - the very beginning!

--- src/main/java/ru/gb/demo_chat/NetworkChat.java
+++ src/main/java/ru/gb/demo_chat/NetworkChat.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class NetworkChat extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(NetworkChat.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 756, 580);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +20,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
